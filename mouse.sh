ae2adcd97ae87b29b92f4c217a8c4cd2d9dff072
Young Li <li.young1618@gmail.com>
2017-01-27 00:17:46 -0600

    Improve mouse.sh for my gaming mouse

--- mouse.sh
+++ mouse.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+xinput set-prop $1 "Device Accel Constant Deceleration" 1
 xinput set-prop $1 "Device Accel Velocity Scaling" 0.0001
-xinput set-prop $1 "Device Accel Constant Deceleration" 2
+xinput set-prop $2 "Device Accel Constant Deceleration" 1
+xinput set-prop $2 "Device Accel Velocity Scaling" 0.0001
 
-xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Velocity Scaling" 0.0001
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Velocity Scaling" 0.0001
